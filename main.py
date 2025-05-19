c4612e47bb81f7a1216df8de05c18a0eec55156c
s26river <51350226+s26river@users.noreply.github.com>
2025-05-19 14:21:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -16,5 +16,5 @@ async def konbanwa(name: str) -> str:
 #mcp.mount()
 
 if __name__ == "__main__":
-    #mcp.run(transport="stdio")
-    mcp.run(transport="sse")
+    mcp.run(transport="stdio")
+    #mcp.run(transport="sse")
