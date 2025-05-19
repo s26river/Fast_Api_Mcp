89c7cc6d73cf9dab0ca88a6eda41753ffae80780
s26river <51350226+s26river@users.noreply.github.com>
2025-05-19 14:22:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,10 @@
 from fastmcp import FastMCP
 import os
 
-#app = FastAPI(title="シンプルAPI")
 mcp = FastMCP()
-
-#@app.get("/hello", operation_id="say_hello")
-@mcp.tool()
 async def konbanwa(name: str) -> str:
     """あいさつ"""
     return f"わんばんこ！, {name}!"
-# MCPサーバーを作成
-#mcp = FastApiMCP(app, name="シンプルMCPサービスです。")
-#mcp.mount()
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
