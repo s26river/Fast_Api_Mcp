16c146c9bfb45828b6fd34aad8505479dcb0e7f6
s26river <51350226+s26river@users.noreply.github.com>
2025-05-19 14:59:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ async def konbanwa(name: str) -> str:
     """あいさつ"""
     return f"わんばんこ！, {name}!"
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
     mcp.run(transport="stdio")
     #mcp.run(transport="sse",host="0.0.0.0")
 
